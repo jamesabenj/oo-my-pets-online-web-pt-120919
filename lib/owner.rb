78bfcb32fa66009ffe32e89490564de232943d3e
James Benjamin <james.abenj@gmail.com>
2020-02-17 01:25:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
+require 'pry'
 class Owner
+  attr_reader :name, :species
+
+  @@all = []
+
+  def initialize(name, species = 'human')
+  @name = name
+  @species = species
+  @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+  Cat.all.select {|cat| cat.owner = self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner = self}
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name, self.name)
+  end
   # code goes here
 end
