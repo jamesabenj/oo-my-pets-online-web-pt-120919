9fbf3e0bf65929bb8f30f9c69e6ccbf559db9b43
James Benjamin <james.abenj@gmail.com>
2020-02-17 01:27:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,11 +27,11 @@ class Owner
   end
 
   def cats
-  Cat.all.select {|cat| cat.owner = self}
+  Cat.all.select {|cat| cat.owner == self}
   end
 
   def dogs
-    Dog.all.select {|dog| dog.owner = self}
+    Dog.all.select {|dog| dog.owner == self}
   end
 
   def buy_cat(name)
